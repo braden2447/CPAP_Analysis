874f624015e1ba3d605c5f1089bfd746cc698158
Braden Garrison <braden.garrison@duke.edu>
2021-09-23 16:56:24 -0400

    Created data manipulation function to split sample_data and arrange data into arrays of patients

--- cpap_database.py
+++ cpap_database.py
@@ -1,10 +1,18 @@
 def data_read_in():
     sample = open('sample_data.txt', 'r')
     sample_data = sample.read()
-    sample.close
+    sample.close()
     return sample_data
+
+
+def data_manipulation(data):
+    split = data.split("\n")
+    N = 5
+    patient_grouping = [split[i:i+N] for i in range(0, len(split), N)]
+    patient_grouping
+    return patient_grouping
 
 
 if __name__ == '__main__':
     data = data_read_in()
-    print(data)
+    patients = data_manipulation(data)
