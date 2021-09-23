228becff9858a8a71c243bdc8f5cf900b78b0f20
Braden Garrison <braden.garrison@duke.edu>
2021-09-23 18:12:14 -0400

    Fixed pycodestyle errors on most recent commit

--- cpap_database.py
+++ cpap_database.py
@@ -9,23 +9,24 @@ def data_manipulation(data):
     split = data.split("\n")
     N = 5
     patient_group = [split[i:i+N] for i in range(0, len(split), N)]
-    patient_group = patient_group[0:-2] # Deleting 'END' array item
+    patient_group = patient_group[0:-2]  # Deleting 'END' array item
     for x in range(len(patient_group)):
-        patient_group[x][1] = float(patient_group[x][1]) # Changing hours of sleep to float
+        patient_group[x][1] = float(patient_group[x][1])
+        # Changing hours of sleep to float
 
-        seal = patient_group[x][2].split(',') # Formatting mask leakage data
+        seal = patient_group[x][2].split(',')  # Formatting mask leakage data
         seal.remove('Seal')
         for y in range(len(seal)):
             seal[y] = float(seal[y])
         patient_group[x][2] = seal
 
-        event = patient_group[x][3].split(',') # Formatting events data
+        event = patient_group[x][3].split(',')  # Formatting events data
         event.remove('Events')
         for y in range(len(event)):
             event[y] = int(event[y])
         patient_group[x][3] = event
 
-        O2 = patient_group[x][4].split(',') # Formatting O2 data
+        O2 = patient_group[x][4].split(',')  # Formatting O2 data
         O2.remove('O2')
         for y in range(len(O2)):
             O2[y] = int(O2[y])
@@ -61,10 +62,9 @@ def data_calculations(data):
 '''def diagnoses(data):
     for i in range(len(data)):
         if len(data[i]) == 5:'''
-    
+
 
 if __name__ == '__main__':
     data = data_read_in()
     patients = data_manipulation(data)
     print(patients)
-    
